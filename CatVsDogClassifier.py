e9a1c868cd2a8b3c70411ebe8f986e833e260471
pratiksharm <sharma.pratik2016@gmail.com>
2019-06-12 16:06:26 +0530

    updating

--- CatVsDogClassifier.py
+++ CatVsDogClassifier.py
@@ -1,6 +1,10 @@
 #here we use division this divison will give us the value with more decimal places.
 #future statement is used to simply the migration to future version of python that in
 #That introduce incompatible changes to the language.
+#TODO: Switch to GPU
+#TODO: image aumentation with this
+#TODO: It may not complete the training So lower the epoch -PAst_pRatik don't get dishearted if that happens
+
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
@@ -36,9 +40,18 @@ num_dogs_val = len(os.listdir(validation_dogs_dir))
 total_train = num_cats_tr + num_dogs_tr
 total_val = num_cats_val + num_dogs_val
 print(total_train, total_val)
+print('total training cat images:', num_cats_tr)
+print('total training dog images:', num_dogs_tr)
 
+print('total validation cat images:', num_cats_val)
+print('total validation dog images:', num_dogs_val)
+print('--')
+print('total training images:',total_train)
+print('total validation images:', total_val)
 BATCH_SIZE = 100
 IMG_SHAPE = 150
+
+
 
 # data preparation
 
@@ -51,19 +64,73 @@ train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE
                                                            shuffle=True,
                                                            target_size=(IMG_SHAPE, IMG_SHAPE),
                                                            class_mode='binary')
-validation_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
                                                                      directory=validation_dir,
                                                                      shuffle=True,
                                                                      target_size=(IMG_SHAPE,IMG_SHAPE),
                                                                      class_mode='binary')
 
 sample_training_images, _ = next(train_data_gen)
+# this function will plot images in the form of a grid with 1 row and 5 columns where images are plac
+
 def plotImages(images_acc):
     fig, axes = plt.subplots(1, 5, figsize =(20,20))
     axes = axes.flatten()
     for img, ax in zip( images_acc, axes):
         ax.imshow(img)
     plt.tight_layout()
-    plt.show()
+    return plt.show()
 
-plotImages(sample_training_images[:5])
+#Model Creation
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32,(3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPool2D(2,2),
+
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2),
+
+    tf.keras.layers.Conv2D(128,(3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2,2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512,activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+EPOCHS = 20
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(total_train/float(BATCH_SIZE))),
+    epochs=EPOCHS,
+    validation_data = val_data_gen,
+    validation_steps=int(np.ceil(total_val/float(BATCH_SIZE)))
+)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range, acc,label='Training Accuracy')
+plt.plot(epochs_range, acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1,2,2)
+plt.plot(epochs_range, loss, label='Training loss')
+plt.plot(epochs_range, val_loss, label='Validation loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation loss')
+plt.savefig('./foo.png')
+plt.show()
